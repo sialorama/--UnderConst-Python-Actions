c79f9c8a2cbf0cbb8f315df3ce36bf21b71ca307
Jamal_Ibanni <60659076+sialorama@users.noreply.github.com>
2023-02-27 12:04:55 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,4 @@
 from app import index
-import os
-
-def set_action_output(output_name, value) :
-    """Sets the GitHub Action output.
-
-    Keyword arguments:
-    output_name - The name of the output
-    value - The value of the output
-    """
-    if "GITHUB_OUTPUT" in os.environ :
-        with open(os.environ["GITHUB_OUTPUT"], "a") as f :
-            print("{0}={1}".format(output_name, value), file=f)
 
 def test_index():
     assert index() == "Hello, world!"
